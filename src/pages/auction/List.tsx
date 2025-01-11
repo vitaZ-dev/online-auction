a6e811d2b4c55ec2a875ee29ec3330ad600da191
vitaZ-dev <vitaminzdev@gmail.com>
2025-01-11 18:27:47 +0900

    json-server 라이브러리 연동

--- src/pages/auction/List.tsx
+++ src/pages/auction/List.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function List() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await axios.get("http://localhost:4000/posts");
+      setPosts(response.data);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <>
       <h1>List</h1>
+      <ul>
+        {posts?.map((post) => {
+          return <li key={post.id}>{post.title}</li>;
+        })}
+      </ul>
     </>
   );
 }
