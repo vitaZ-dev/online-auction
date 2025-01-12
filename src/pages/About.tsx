8095f1e9fb4740ebac025f6a5249e34cc84fce51
vitaZ-dev <vitaminzdev@gmail.com>
2025-01-12 22:27:34 +0900

    zustand 설치 및 테스트

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,15 @@
+import useCounterStore from "../stores/useCounterStore";
+
 export default function About() {
+  const { count, increase, decrease } = useCounterStore();
   return (
     <>
       <h1>about</h1>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={increase}>Increase</button>
+        <button onClick={decrease}>Decrease</button>
+      </div>
     </>
   );
 }
