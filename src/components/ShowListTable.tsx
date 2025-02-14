da169f2bfff36daba8feedb0c79a78c861d5ad4a
vitaZ-dev <vitaminzdev@gmail.com>
2025-02-14 17:35:20 +0900

    커스텀 UI 컴포넌트 제작(테이블리스트)

---
+++ src/components/ShowListTable.tsx
@@ -0,0 +1,29 @@
+import { ShowListTableLayout } from "../styles/TempStyle";
+import { moneyFormat } from "../utils";
+
+export default function ShowListTable({ tableGrid, tableHeader, tableList }) {
+  return (
+    <ShowListTableLayout tableGrid={tableGrid}>
+      <div className="table_header">
+        {tableHeader?.map((header, idx) => (
+          <span key={`table_header_${idx}`}>{header}</span>
+        ))}
+      </div>
+      <div className="table_contents">
+        {tableList?.map((list, idx) => (
+          <p key={`table_list_${idx}`}>
+            {Array(tableHeader.length)
+              .fill(0)
+              .map((_, index) => (
+                <span key={`table_list_${idx}_${index}`}>
+                  {Number.isInteger(list[tableHeader[index]])
+                    ? moneyFormat(list[tableHeader[index]])
+                    : list[tableHeader[index]]}
+                </span>
+              ))}
+          </p>
+        ))}
+      </div>
+    </ShowListTableLayout>
+  );
+}
---
+++ src/styles/TempStyle.ts
@@ -0,0 +1,33 @@
+import styled from "@emotion/styled";
+
+export const ShowListTableLayout = styled.div`
+  position: relative;
+
+  .table_header,
+  .table_contents > * {
+    display: grid;
+    grid-template-columns: ${({ tableGrid }) =>
+      tableGrid.map((grid) => `${grid}fr`).join(" ")};
+
+    line-height: 2;
+  }
+
+  .table_header {
+    position: sticky;
+    top: 0;
+    left: 0;
+    width: 100%;
+    border-bottom: 1px solid red;
+    span {
+      padding: 0 8px;
+    }
+  }
+
+  .table_contents {
+    height: 70vh;
+    overflow-y: auto;
+    span {
+      padding: 0 8px;
+    }
+  }
+`;
