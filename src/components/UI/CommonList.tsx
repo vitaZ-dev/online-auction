19467751f8c19e89bcbb27cefb77d3883a8d17e4
vitaZ-dev <vitaminzdev@gmail.com>
2025-03-06 23:32:29 +0900

    데이터 로딩 중 스켈레톤 UI 적용

--- src/components/UI/CommonList.tsx
+++ src/components/UI/CommonList.tsx
@@ -1,15 +1,46 @@
+import { Skeleton } from "@mui/material";
 import { AuctionListLayout } from "../../styles/CommonStyle";
+import { useEffect, useState } from "react";
 
 export default function CommonList({
   grid = 2,
+  loading = false,
   children,
 }: {
   grid?: number;
+  loading?: boolean;
   children;
 }) {
+  const [height, setHeight] = useState(200);
+
+  useEffect(() => {
+    grid === 2 ? setHeight(200) : setHeight(100);
+  }, [grid]);
+
   return (
     <>
-      <AuctionListLayout grid={grid}>{children}</AuctionListLayout>
+      <AuctionListLayout grid={grid}>
+        {loading ? (
+          Array(4)
+            .fill(0)
+            .map((_, idx) => (
+              <>
+                <div key={idx}>
+                  <Skeleton
+                    variant="rounded"
+                    height={height}
+                    animation="wave"
+                  />
+                  <Skeleton width={"30%"} height={20} animation="wave" />
+                  <Skeleton variant="text" height={16} animation="wave" />
+                  <Skeleton variant="text" height={16} animation="wave" />
+                </div>
+              </>
+            ))
+        ) : (
+          <>{children}</>
+        )}
+      </AuctionListLayout>
     </>
   );
 }
