d65741e6ce78f6725f89eeaaf3fd91206d9ec6eb
vitaZ-dev <vitaminzdev@gmail.com>
2025-03-14 02:34:59 +0900

    main padding-top 값 수정

--- src/styles/AppStyle.ts
+++ src/styles/AppStyle.ts
@@ -62,7 +62,7 @@ const MainLayout = styled.main`
   min-height: calc(100vh - 100px);
   height: 100%;
   /* padding: 72px 32px 32px; */
-  padding-top: 72px;
+  padding-top: calc(72px + 8px);
 `;
 
 export const FooterLayout = styled.footer`
