3ce4e2c1168ed2aa59b40264248c6a4efef46f87
vitaZ-dev <vitaminzdev@gmail.com>
2025-03-17 22:31:08 +0900

    페이지로딩 loader pure css로 작업

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,33 @@
 export default function Loading() {
   return (
-    <>
-      <h1>Loading</h1>
-    </>
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: " #4b44444d",
+        zIndex: 9999,
+      }}
+    >
+      <div className="loding_spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   );
 }
