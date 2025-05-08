3fbfe14f79c0560840c77fce55b45adb069ff7ea
vitaZ-dev <vitaminzdev@gmail.com>
2025-05-08 18:08:44 +0900

    경매 가이드 페이지 UI작업 및 내용 추가

--- src/styles/ServiceStyle.ts
+++ src/styles/ServiceStyle.ts
@@ -1,5 +1,33 @@
 import styled from "@emotion/styled";
 
+export const GuideLayout = styled.div`
+  padding-bottom: var(--padding-size);
+
+  section {
+    margin: var(--padding-size) 0;
+    padding: 0 var(--padding-size);
+
+    .head_title {
+      font-size: 17px;
+      margin-bottom: 4px;
+      &::before {
+        content: "";
+        display: block;
+        width: 20px;
+        height: 3px;
+        margin-bottom: 3px;
+        background: var(--main-violet-02-color);
+      }
+    }
+    p {
+      line-height: 1.4;
+      &.in {
+        padding-left: 4px;
+      }
+    }
+  }
+`;
+
 export const ServiceLayout = styled.div`
   padding: 0 var(--padding-size);
   margin-bottom: 24px;
