61d1c805e244fa8a11786a3fb1a57b662a32692a
vitaZ-dev <vitaminzdev@gmail.com>
2025-05-08 16:18:33 +0900

    좋아요 데이터 firestore 저장 시 id값 게시글 아이디로 수정

--- src/pages/auction/Detail.tsx
+++ src/pages/auction/Detail.tsx
@@ -218,7 +218,7 @@ export default function Detail() {
 
     try {
       const addItem = {
-        id: userInfo?.id + POST_ID!,
+        id: POST_ID!,
         item_id: POST_ID!,
         user_id: userInfo?.id as string,
         uuid: userInfo?.uuid as string,
