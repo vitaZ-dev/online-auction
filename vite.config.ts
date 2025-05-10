c346c1df13dbfcfd8010ece8fb4b025fb707212a
vitaZ-dev <vitaminzdev@gmail.com>
2025-05-10 15:30:34 +0900

    빌드&배포 준비 - 테스트 용 작업 코드 위치 이동 - 테스트 코드 타입체크 및 빌드 제외 설정 - 빌드 시 chunk 사이즈 최적화: 라이브러리 별 코드 분리 - 미사용 라이브러리 제거 - 배포 버전 설정

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,38 @@ export default defineConfig({
   // resolve: {
   //   alias: [{ find: "@", replacement: "/src" }],
   // },
+  build: {
+    rollupOptions: {
+      external: (id) => id.includes("src/dev-only"),
+      output: {
+        assetFileNames: `assets/[name].v100[extname]`,
+        chunkFileNames: `assets/[name].v100.js`,
+        entryFileNames: `assets/[name].v100.js`,
+        manualChunks(id: string) {
+          if (
+            id.includes("node_modules/react/") ||
+            id.includes("node_modules/react-dom/") ||
+            id.includes("node_modules/react-router-dom/")
+          ) {
+            return "@react-vendor";
+          }
+          if (
+            id.includes("@mui/icons-material") ||
+            id.includes("@mui/material")
+          ) {
+            return "@mui-vendor";
+          }
+          if (id.includes("firebase")) {
+            return "@firebase";
+          }
+          if (id.includes("component")) {
+            return "@component";
+          }
+          if (id.includes("@tanstack/react-query")) {
+            return "@react-query";
+          }
+        },
+      },
+    },
+  },
 });
